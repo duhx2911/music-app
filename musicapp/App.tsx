4436f56c0901c69e83a22b90bfefc636c92ba8cb
duhx2911 <nxd2911@gmail.com>
2023-09-22 08:51:17 +0700

    Add app icon, splash screen

--- musicapp/App.tsx
+++ musicapp/App.tsx
@@ -10,8 +10,16 @@ import React from 'react';
 import MainRouter from './src/routes';
 import AuthProvider from './src/context';
 import {ModalPortal} from 'react-native-modals';
+import {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import {Platform} from 'react-native';
 
 const App = () => {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
   return (
     <AuthProvider>
       <MainRouter />
